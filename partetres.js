c707cdb9c9d6ed1e95d9509fa66d652a3dc5a6a5
Isis Menezes <isisalternativo@gmail.com>
2025-03-05 02:44:01 -0300

    modificacoes extras

--- partetres.js
+++ partetres.js
@@ -1,27 +1,8 @@
-    function setupSlider(sliderClass) {
-    const slider = document.querySelector(sliderClass);
-    const sliderContent = slider.querySelector('.slider-content');
-    const slides = slider.querySelectorAll('.slide');
-    const leftArrow = slider.querySelector('.left-arrow');
-    const rightArrow = slider.querySelector('.right-arrow');
-    
-    let currentIndex = 0; 
-    
-    function updateSlider() {
-        const offset = -currentIndex * 100;
-        sliderContent.style.transform = `translateX(${offset}%)`; 
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
     }
-    
-    leftArrow.addEventListener('click', () => {
-        currentIndex = (currentIndex > 0) ? currentIndex - 1 : slides.length - 1;
-        updateSlider();
-    });
-    
-    rightArrow.addEventListener('click', () => {
-        currentIndex = (currentIndex < slides.length - 1) ? currentIndex + 1 : 0;
-        updateSlider();
-    });
 }
-
-setupSlider('.projetos-slider');
-setupSlider('.experiencia-slider');
